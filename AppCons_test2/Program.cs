3be75acdbba63051ea724b19418528d348a6db20
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2020-05-25 09:30:57 -0500

    ejecución secuencial

--- AppCons_test2/Program.cs
+++ AppCons_test2/Program.cs
@@ -11,7 +11,17 @@ namespace AppCons_test2
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo c#");
+            
+
+            //ejemplo de los pilares de la programación estructurada
+            //secuencias
+            int a;
+            a= 10;
+            int b = 20;
+            Console.WriteLine("la multiplicación de {0} x {1} es {2}", a, b, (a * b));
             Console.WriteLine("Pulse una tecla para continuar...");
+            Console.ReadKey();
+
         }
     }
 }
